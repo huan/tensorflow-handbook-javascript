08cb0a9cde565bb9a96cfb63a012591b620594c2
Huan LI <zixia@zixia.net>
2019-09-22 16:27:31 +0800

    keep orignal gzip file by --keep

--- scripts/download.sh
+++ scripts/download.sh
@@ -10,5 +10,5 @@ pushd dist
 # [ -e fra-eng.zip ] || curl -L -O http://www.manythings.org/anki/fra-eng.zip
 # [ -e 'fra.txt' ]   || unzip -o fra-eng.zip
 [ -e dataset.txt.gz ] || curl -L -O https://github.com/huan/python-concise-chitchat/releases/download/v0.0.1/dataset.txt.gz
-[ -e dataset.txt ] || gzip -d dataset.txt
+[ -e dataset.txt ] || gzip --keep -d dataset.txt.gz
 popd
